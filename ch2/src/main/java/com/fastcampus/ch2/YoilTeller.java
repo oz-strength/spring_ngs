c0dfedf2ad896059b6c5333b25e4422bea3c7db3
Seungwoo Oh <sw6016@hanmail.net>
2023-05-26 01:42:56 +0900

    http 요청과 응답 실습

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -1,14 +1,24 @@
 package com.fastcampus.ch2;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Calendar;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 // 년월일을 입력하면 요일을 알려주는 프로그램
-public class YoilTeller {
- public static void main(String[] args) {
+@Controller
+public class YoilTeller { // http://localhost:8080/ch2/getYoil?year=2021&month=10&day=1
+	@RequestMapping("/getYoil")
+	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
 	// 1.입력
-	 String year = args[0];
-	 String month = args[1];
-	 String day = args[2];
+	 String year = request.getParameter("year");
+	 String month = request.getParameter("month");
+	 String day = request.getParameter("day");
 	 
 	 int yyyy = Integer.parseInt(year);
 	 int mm = Integer.parseInt(month);
@@ -22,8 +32,11 @@ public class YoilTeller {
 	 char yoil = " 일월화수목금토".charAt(dayOfWeek);
 	 
 	 // 3. 출력
-	 System.out.println(year + "년" + month + "월" + day + "일은");
-	 System.out.println(yoil + "요일입니다.");
+	 response.setContentType("text/html");
+	 response.setCharacterEncoding("utf-8");
+	 PrintWriter out = response.getWriter(); // response객체에서 브라우저로의 출력 스트림을 얻는다.
+	 out.println(year + "년" + month + "월" + day + "일은");
+	 out.println(yoil + "요일입니다.");
 			 
 }
 }
