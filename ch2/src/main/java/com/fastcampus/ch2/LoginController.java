5f336285173d33bac335b9edebed8c5f9aa8ca9f
Seungwoo Oh <sw6016@hanmail.net>
2023-07-08 05:00:32 +0900

    login까지 완료

--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -3,9 +3,12 @@ package com.fastcampus.ch2;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +20,17 @@ public class LoginController {
 	public String loginForm() {
 		return "loginForm";
 	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		// 1. 세션을 종료
+		session.invalidate();
+		// 2. 홈으로 이동
+		return "redirect:/";
+	}
+	
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
+	public String login(@CookieValue("id") String cookieId, String id, String pwd, String toURL, boolean rememberId, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("id="+id);
 		System.out.println("pwd="+pwd);
 		System.out.println("rememberId="+rememberId);
@@ -30,7 +42,10 @@ public class LoginController {
 			 return "redirect:/login/login?msg="+msg;
 		 }
 		//	2-2. id와 pwd가 일치하면, 
-		 
+		 // 세션 객체를 얻어오기
+		 HttpSession session = request.getSession();
+		 // 세션 객체에 id를 저장
+		 session.setAttribute("id", id);
 		 if(rememberId) {
 			 // 	1. 쿠키를 생성
 			 Cookie cookie = new Cookie("id", id); 
@@ -45,7 +60,9 @@ public class LoginController {
 			 response.addCookie(cookie);
 		 }
 		 //		3. 홈으로 이동 
-		return "redirect:/";
+		 toURL = toURL==null || toURL.equals("") ? "/" : toURL;
+		 
+		 return "redirect:"+toURL;
 	}
 	private boolean loginCheck(String id, String pwd) {
 		
